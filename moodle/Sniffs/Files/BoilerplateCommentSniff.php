952aa317a0717df71f6ae6a43621a16da43b370c
George Mihailov <mihailoff@gmail.com>
2016-09-21 17:28:11 -0700

    Remove Moodle GPL license check
    There are other GPL compatible licenses

--- moodle/Sniffs/Files/BoilerplateCommentSniff.php
+++ moodle/Sniffs/Files/BoilerplateCommentSniff.php
@@ -23,22 +23,7 @@
  */
 
 class moodle_Sniffs_Files_BoilerplateCommentSniff implements PHP_CodeSniffer_Sniff {
-    protected static $comment = array(
-        "// This file is part of",
-        "//",
-        "// Moodle is free software: you can redistribute it and/or modify",
-        "// it under the terms of the GNU General Public License as published by",
-        "// the Free Software Foundation, either version 3 of the License, or",
-        "// (at your option) any later version.",
-        "//",
-        "// Moodle is distributed in the hope that it will be useful,",
-        "// but WITHOUT ANY WARRANTY; without even the implied warranty of",
-        "// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the",
-        "// GNU General Public License for more details.",
-        "//",
-        "// You should have received a copy of the GNU General Public License",
-        "// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.",
-    );
+    protected static $comment = array();
     public function register() {
         return array(T_OPEN_TAG);
     }
